2089c56c589eee397b5a28687d2479a3eccbf516
umaimayousuf17 <159889906+umaimayousuf17@users.noreply.github.com>
2024-05-14 10:45:35 +0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Calculator-Project-using-TypeScript-Node.js" 
---
+++ index.ts
@@ -0,0 +1,35 @@
+import inquirer from "inquirer";
+
+// Asking Questions from Users through Inquirer
+let answers = await inquirer.prompt([
+  {
+    message: "Enter First Number",
+    type: "number",
+    name: "firstNumber"
+  },
+  {
+    message: "Enter Second Number",
+    type: "number",
+    name: "secondNumber"
+  },
+  {
+    message: "Select one Operator to perform Operations",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtractions", "Multiplication", "Division"]
+  }
+]);
+
+// Condtional statements If-Else
+if (answers.operator === "Addition") {
+  console.log(answers.firstNumber + answers.secondNumber);
+} else if (answers.operator === "Subtractions") {
+  console.log(answers.firstNumber - answers.secondNumber);
+} else if (answers.operator === "Multiplication") {
+  console.log(answers.firstNumber * answers.secondNumber);
+} else if (answers.operator === "Division") {
+  console.log(answers.firstNumber / answers.secondNumber);
+} else {
+  console.log("Invalid Input");
+}
+
